2a626587ac789354b58361918eff8963df5cfb61
MohamedAhmed <mo9958158@gmail.com>
2025-07-23 15:09:15 +0300

    feat: Splash Screen

---
+++ app/src/main/java/com/mohamed/routeshop/ui/theme/colors.kt
@@ -0,0 +1,9 @@
+package com.mohamed.routeshop.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object colors {
+    val Blue = Color(0xFF0048FF)
+    val LightBlue = Color(0xFF407BFF)
+    val DarkGreen = Color(0xFF156651)
+}
